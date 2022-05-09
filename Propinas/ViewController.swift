73c8a2c0e6ba02047791fffd6ad53d47c1a2f8c3
areyes403 <areyes403>
2022-05-09 09:12:54 -0500

    Initial Commit

---
+++ Propinas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Propinas
+//
+//  Created by mac17 on 09/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
