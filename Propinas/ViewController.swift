d30b242adb29e7c027b4eadfd65ff58ac563b776
areyes403 <areyes403>
2022-05-09 09:57:12 -0500

    First Commit

--- Propinas/ViewController.swift
+++ Propinas/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfTotal: UITextField!
+    @IBOutlet weak var slCantidad: UISlider!
+    @IBOutlet weak var tfNumPersonas: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+    }
+    
 }
 
